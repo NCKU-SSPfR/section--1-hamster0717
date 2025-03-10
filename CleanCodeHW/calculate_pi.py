58a47f4c30d95c7cbff9f42a0029dadb436db0ef
hamster0717 <aoscar0717@gmail.com>
2025-03-10 22:59:01 +0800

    fix: wrong space

--- CleanCodeHW/calculate_pi.py
+++ CleanCodeHW/calculate_pi.py
@@ -1,21 +1,20 @@
 import random
 
-# Constants
 RADIUS = 1
-NUM_POINTS = 1_000_000  # Total number of points to generate
-AREA_MULTIPLIER = 4  # Factor to scale the circle area
-SQUARE_EXPONENT = 2  # Exponent for squaring values
+NUM_POINTS = 1_000_000 
+AREA_FACTOR = 4
+SQUARE_EXPONENT = 2
+POINT_COUNT_INCREMENT = 1
 
-points_inside_circle = 0
+inside_circle = 0
 
-# Monte Carlo method to estimate Pi
 for _ in range(NUM_POINTS):
-    x_coordinate = random.uniform(-RADIUS, RADIUS)
-    y_coordinate = random.uniform(-RADIUS, RADIUS)
-    if x_coordinate**SQUARE_EXPONENT + y_coordinate**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
-        points_inside_circle += 1
+    x = random.uniform(-RADIUS, RADIUS)
+    y = random.uniform(-RADIUS, RADIUS)
+    
+    if x**SQUARE_EXPONENT + y**SQUARE_EXPONENT <= RADIUS**SQUARE_EXPONENT:
+        inside_circle += POINT_COUNT_INCREMENT
 
-# Compute Pi approximation
-estimated_pi = (points_inside_circle / NUM_POINTS) * AREA_MULTIPLIER
+estimated_pi = (inside_circle / NUM_POINTS) * AREA_FACTOR
 
 print(f"Estimated value of pi is: {estimated_pi}")
